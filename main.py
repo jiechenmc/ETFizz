af21a7ae6f93c6bea889c0adbff621d93aa05fd0
Jie Chen <jiechenmc@gmail.com>
2024-01-04 11:27:11 -0500

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+# Target
+# Summary -> Holdings -> Performance
+
+# Summary
+# Price -> Expense Ratio -> Yield -> PE 
+# -- High PE means that STOCK PRICE > EARNINGS
+
+# Holdings
+# Sector -> Top 10 Holdings
+
+# Performance
+# 1,3,5,10 Year Performance
