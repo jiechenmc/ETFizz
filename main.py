24753668b884edc1b8525aaec7b5bf3dfc52f1cc
Jie Chen <jiechenmc@gmail.com>
2024-01-04 11:30:48 -0500

    incl target links

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 # Target
 # Summary -> Holdings -> Performance
 
-# Summary
+# Summary 
+# https://finance.yahoo.com/quote/QQQ?p=QQQ
 # Price -> Expense Ratio -> Yield -> PE 
 # -- High PE means that STOCK PRICE > EARNINGS
 
 # Holdings
+# https://finance.yahoo.com/quote/QQQ/holdings?p=QQQ
 # Sector -> Top 10 Holdings
 
 # Performance
+# https://finance.yahoo.com/quote/QQQ/performance?p=QQQ
 # 1,3,5,10 Year Performance
+
+# Fund Discovery
+# https://www.morningstar.com/5-star-etfs
